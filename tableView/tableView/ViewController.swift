63ba2900a995cf2fb9f4f1b14d1ba81eed471437
minalborole26 <85941305+minalborole26@users.noreply.github.com>
2021-06-17 18:08:53 +0530

    Add files via upload

---
+++ tableView/tableView/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by minal borole on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "iOS"
+        cell?.detailTextLabel?.text = "Felix Its"
+        return cell!
+    }
+    
+    
+}
